b07de97da64c49c3ea3e35967600312c066191ef
MitchGianoni <mitchgianoni@yahoo.com>
2018-07-23 17:13:28 -0400

    initial commit

---
+++ src/components/game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Nav from './nav';
+
+class Game extends Component {
+  render() {
+    return (
+      <header><Nav /><h1>HOT or COLD</h1></header>
+    );
+  }
+}
+
+export default Game;
---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Nav(props) {
+	return (
+		<nav>
+			<ul>
+				<li><a href="#what" className="what"> WHAT? </a></li>
+				<li>+NEW GAME</li>
+			</ul>
+		</nav>
+	);
+}
